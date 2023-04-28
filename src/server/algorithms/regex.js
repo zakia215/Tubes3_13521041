b65376006831fd684d6e9983f402e7eebbbde459
Zaki <zakiamanullah215@gmail.com>
2023-04-28 14:22:20 +0700

    feat: regex

--- src/server/algorithms/regex.js
+++ src/server/algorithms/regex.js
@@ -1,3 +1,22 @@
-const operations = /^(\s*\d+(\.\d+)?\s*([-+*/]\s*\d+(\.\d+)?\s*)*\s*|\s*\(\s*(\s*\d+(\.\d+)?\s*([-+*/]\s*\d+(\.\d+)?\s*)*\s*)+\s*\)\s*)+$/g
-const date = /^(0?[1-9]|[1-2][0-9]|3[0-1])\/(0?[1-9]|1[0-2])\/\d{4}$/g
-const del_question = /^hapus pertanyaan .*$/i;
+// tinggal .match() kalo mau coockin
+const operations = /^(\s*\d+(\.\d+)?\s*([-+*/]\s*\d+(\.\d+)?\s*)*\s*)+&/ig
+const date = /(0?[1-9]|[1-2][0-9]|3[0-1])\/(0?[1-9]|1[0-2])\/\d{4}/g
+const delQuestion = /^hapus pertanyaan .*$/i;
+const add_question = /^tambah pertanyaan . dengan jawaban .$/i;
+
+function hasMathProperties(string) {
+    const mathProperties = /([\s\d-.+/*()])+/ig
+    const matches = string.match(mathProperties);
+    const result = []
+    for (let match of matches) {
+        const trimmed = match.trim();
+        if (trimmed !== '') {
+            result.push(trimmed);
+        }
+    }
+    return result;
+}
+
+function isValidDate(string) {
+    // string is in the format of dd//mm//yyyy and is to be checked
+}
