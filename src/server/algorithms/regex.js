0e8c1971ba42a230324484d764bcce90bc8c4b68
Zaki <zakiamanullah215@gmail.com>
2023-04-28 08:50:18 +0700

    feat: tambah regex

--- src/server/algorithms/regex.js
+++ src/server/algorithms/regex.js
@@ -1,3 +1,3 @@
 const operations = /^(\s*\d+(\.\d+)?\s*([-+*/]\s*\d+(\.\d+)?\s*)*\s*|\s*\(\s*(\s*\d+(\.\d+)?\s*([-+*/]\s*\d+(\.\d+)?\s*)*\s*)+\s*\)\s*)+$/g
 const date = /^(0?[1-9]|[1-2][0-9]|3[0-1])\/(0?[1-9]|1[0-2])\/\d{4}$/g
-
+const del_question = /^hapus pertanyaan .*$/i;
