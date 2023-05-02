87ce791a8ac75ea1489debb00d6eec5c7bba32a9
Zaki <zakiamanullah215@gmail.com>
2023-05-02 16:20:17 +0700

    feat: logic

--- src/server/middleware/get-answer.js
+++ src/server/middleware/get-answer.js
@@ -11,13 +11,33 @@ const {
     getDay
 } = require('../algorithms/regex');
 
-function get_answer_string(question, question_db) {
+function get_answer_string(question, question_db, is_kmp) {
     const is_date = question.match(date);
     const has_math_prop = question.match(hasMathProperties);
     const is_del_question = question.match(del_question);
-    const add_question = question.match(add_question);
+    const is_add_question = question.match(add_question);
 
     if (is_date) {
+        if (isValidDate(is_date[0])) {
+            return getDay(is_date[0]);
+        } else {
+            return "Tanggal pada pertanyaan tidak valid";
+        }
+    } else if (has_math_prop) {
         
+    } else if (is_del_question) {
+
+    } else if (is_add_question) {
+
+    } else {
+        let found = false;
+        if (is_kmp) {
+
+        } else {
+
+        }
+        if (!found) {
+            
+        }
     }
 }
