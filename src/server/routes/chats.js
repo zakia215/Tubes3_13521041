6d120a8cf8afb06c5664fa3e2aad544cd7554025
Zaki <zakiamanullah215@gmail.com>
2023-05-03 16:23:54 +0700

    deploy: g tau lagi

--- src/server/routes/chats.js
+++ src/server/routes/chats.js
@@ -8,7 +8,9 @@ const {
     updateChat
 } = require('../controllers/chats');
 
+// api/chat/
 router.route('/').get(getAllChat).post(createChat);
+// api/chat/:id
 router.route('/:id').get(getSingleChat).patch(updateChat).delete(deleteChat);
 
 module.exports = router;
